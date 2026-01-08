7449f203e06936b154119df7b2a016afd7ab273a
yuta <yutasosukeaoto1016@gmail.com>
2026-01-08 10:33:17 +0900

    xmlのビルドが上手くいってない

---
+++ MPEG7/w15127SW/xml/build/CTestTestfile.cmake
@@ -0,0 +1,10 @@
+# CMake generated Testfile for 
+# Source directory: C:/Users/USER/Desktop/SchoolRelation/GraduationReaserch/Scraping/MPEG7/w15127SW/xml
+# Build directory: C:/Users/USER/Desktop/SchoolRelation/GraduationReaserch/Scraping/MPEG7/w15127SW/xml/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("doc")
+subdirs("src")
+subdirs("tests")
+subdirs("samples")
